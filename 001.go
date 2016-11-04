348e543059c3012ab064220749c6256654fb700f
Ben McCormack <ben@fullstory.com>
2016-11-04 00:40:53 +0000

    I don't like go yet. Look at all those extra lines!

--- 001.go
+++ 001.go
@@ -5,5 +5,12 @@ import (
 )
 
 func main() {
-	fmt.Println("Hello World")
+	sum := 0
+	for i :=0; i < 1000; i++ {
+	    if i % 3 == 0 || i % 5 == 0 {
+	        sum += i	        
+	    }
+	}
+	
+	fmt.Println(sum)
 }
--- 001.py
+++ 001.py
@@ -1,1 +1,8 @@
-print "hello world"
+#Find the sum of all the multiples of 3 or 5 below 1000.
+
+sum = 0
+
+for i in range(1,1000):
+    sum += i if 0 in [i % 3, i % 5] else 0
+    
+print sum
